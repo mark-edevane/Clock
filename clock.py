26cac5e4d019f4a5eae1e20f502fed5470733948
mark-edevane <mark.pribylnov@gmail.com>
2022-06-30 19:17:42 +0300

    Stopwatch works. Next: fix buttons

--- clock.py
+++ clock.py
@@ -75,28 +75,37 @@ set_alarm_button.pack()
 alarm_status_label = Label(alarm_tab, font = 'calibri 10', text = '')
 alarm_status_label.pack()
 
-# Stopwatch components
-stopwatch_label = Label(stopwatch_tab, font='calibri 15', text='Stopwatch')
-stopwatch_label.pack()
-stopwatch_start = Button(stopwatch_tab, text='Start', command=lambda:stopwatch_func('start'))
-stopwatch_start.pack()
-stopwatch_stop = Button(stopwatch_tab, text='Stop', state='disabled',command=lambda:stopwatch_func('stop'))
-stopwatch_stop.pack()
-stopwatch_reset = Button(stopwatch_tab, text='Reset', state='disabled', command=lambda:stopwatch_func('reset'))
-stopwatch_reset.pack()
-
 # Stopwatch function
-
 def stopwatch_func():
     start_time = time.time()
-    mins = sec // 60
-    sec = sec % 60
-    hours = mins // 60
-    mins = mins % 60
-    print("Time Lapsed = {0}:{1}:{2}".format(int(hours),int(mins),sec))
-    stopwatch_label.after(1000, stopwatch_func)
-time_convert(time_lapsed)
-# ADD 1 SECOND AND FIND THE WAY HOW TO CONVERT IT TO TICKING CLOCK FROM TICKING SECONDS
+#   print(start_time)
+    def stopwatch_ongoing():
+        def time_convert(sec):
+            mins = sec // 60
+            sec = sec % 60
+            hours = mins // 60
+            mins = mins % 60
+            return sec, mins, hours
+        end_time = time.time()
+        time_lapsed = end_time - start_time
+        secs = int(time_convert(time_lapsed)[0])
+        mins =  int(time_convert(time_lapsed)[1])
+        hours =  int(time_convert(time_lapsed)[2])
+        stopwatch_label.config(text = "Time lapsed = {0}:{1}:{2}".format(hours,mins,secs))
+        stopwatch_label.after(1000, stopwatch_ongoing)
+    stopwatch_ongoing()
+
+
+# Stopwatch components
+stopwatch_label = Label(stopwatch_tab, font='calibri 20', text='Stopwatch')
+stopwatch_label.pack()
+stopwatch_start = Button(stopwatch_tab, text='Start', command=stopwatch_func)
+stopwatch_start.pack()
+stopwatch_stop = Button(stopwatch_tab, text='Stop', state='disabled',command=lambda:stopwatch('stop'))
+stopwatch_stop.pack()
+stopwatch_reset = Button(stopwatch_tab, text='Reset', state='disabled', command=lambda:stopwatch('reset'))
+stopwatch_reset.pack()
+
 
 
 # Exit button
@@ -111,6 +120,5 @@ exit_btn.pack(fill = 'x')
 def exit_func(event):
     root.quit()
 root.bind('<Return>', exit_func)
-
 time_func()
 mainloop()
--- test.py
+++ test.py
@@ -1,4 +1,28 @@
-from pygame import mixer
-mixer.init()
-sound=mixer.Sound("bell.wav")
-sound.play()
+def func():
+    x = 1
+    y = 3
+    z = 14
+    return z % y, z // y
+#print(func(), 153 % 60)
+
+import time
+def time_convert(sec):
+ #   print('1 - seconds total =', int(sec))
+    mins = sec // 60
+   # print('2 - min =', mins)
+    sec = sec % 60
+   # print('3 - seconds after getting minutes =', int(sec))
+    hours = mins // 60
+  #  print('4 - hours =', hours)
+    mins = mins % 60
+  #  print('5 - mins 2 =', mins)
+  #  print("Time Lapsed = {0}:{1}:{2}".format(int(hours),int(mins),int(sec)))
+    return sec, mins, hours
+
+# input("Press Enter to start")
+# start_time = time.time()
+# input("Press Enter to stop")
+# end_time = time.time()  
+# time_lapsed = end_time - start_time
+# time_convert(time_lapsed)
+print(time_convert(185)[1])
