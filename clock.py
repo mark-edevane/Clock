36e6d71208cc3bd0b0c84056fad18a65d5096f9a
mark-edevane <mark.pribylnov@gmail.com>
2022-07-16 15:11:57 +0300

    Done

--- clock.py
+++ clock.py
@@ -10,31 +10,32 @@ root.title('Clock')
 tabs = ttk.Notebook(root)
 tabs.pack()
 
-clock_tab = ttk.Frame(tabs)
 alarm_tab = ttk.Frame(tabs)
 timer_tab = ttk.Frame(tabs)
 stopwatch_tab = ttk.Frame(tabs)
 
-tabs.add(timer_tab, text='Timer')
 tabs.add(stopwatch_tab, text='Stopwatch')
-tabs.add(alarm_tab, text='Alarm')
-tabs.add(clock_tab, text='Clock')
+tabs.add(timer_tab, text='Timer')
+tabs.add(alarm_tab, text='Alarm and Clock')
 
 
-# Clock components
-time_label = Label(clock_tab, font = ('calibri', 20))
-time_label.pack()
-date_label = Label(clock_tab, font = ('calibri', 10))
+# Clock components within alarm tab
+clock_label = Label(alarm_tab, font = ('calibri', 20))
+clock_label.pack()
+date_label = Label(alarm_tab, font = ('calibri', 10))
 date_label.pack()
 
+
 # Clock function
-def time_func():
+def clock_func():
     time_str = datetime.datetime.now().strftime("%H:%M:%S")
     date_str = datetime.datetime.now().strftime("%d.%m.%Y")
-    time_label.config(text = time_str)
+    clock_label.config(text = time_str)
     date_label.config(text = date_str)
-    root.after(1000, time_func)
+    root.after(1000, clock_func)
 
+
+# Variable and functions to start and stop the time
 running = False
 
 def start():
@@ -44,7 +45,6 @@ def start():
 def stop():
     global running
     running = False
-
 
 
 # Alarm function
@@ -60,7 +60,6 @@ def alarm_func():
         get_alarm_time_entry.config(state='normal')
         set_alarm_button.config(state='normal')
 
-
     if running:
         alarm_status_label.config(text='Alarm Has Started')
         get_alarm_time_entry.config(state='disabled')
@@ -70,7 +69,7 @@ def alarm_func():
 # Alarm components
 get_alarm_time_entry = Entry(alarm_tab, font = 'calibri 15')
 get_alarm_time_entry.pack()
-alarm_instructions = Label(alarm_tab, font = 'calibri 10', text = 'Enter alarm time. For instance: 10:11:12')
+alarm_instructions = Label(alarm_tab, font = 'calibri 10', text = 'Enter time as HH:MM:SS')
 alarm_instructions.pack()
 set_alarm_button = Button(alarm_tab, text = 'Set alarm', command=lambda: [start(), alarm_func()])
 set_alarm_button.pack()
@@ -78,12 +77,8 @@ alarm_status_label = Label(alarm_tab, font = 'calibri 10', text = '')
 alarm_status_label.pack()
 
 
-
-
 # Stopwatch variables
-seconds = 0
-minutes = 0
-hours = 0
+seconds, minutes, hours = 0, 0, 0
 
 # Stopwatch functions
 def stopwatch_update():
@@ -95,7 +90,7 @@ def stopwatch_update():
     if minutes == 60:
         hours += 1
         minutes = 0
-    
+
     hours_string = f'{hours}' if hours > 9 else f'0{hours}'
     minutes_string = f'{minutes}' if minutes > 9 else f'0{minutes}'
     seconds_string = f'{seconds}' if seconds > 9 else f'0{seconds}'
@@ -128,7 +123,6 @@ def stopwatch_func(command):
         stopwatch_label.config(text='00:00:00')
         stopwatch_label.after_cancel(stopwatch_after)
 
-        
 
 # Stopwatch components
 stopwatch_label = Label(stopwatch_tab, font='calibri 20', text='Stopwatch')
@@ -141,60 +135,86 @@ stopwatch_reset = Button(stopwatch_tab, text='Reset', state='disabled', command=
 stopwatch_reset.pack()
 
 
-
-
-
 # Timer functions
-
-t_seconds = 0
-t_minutes = 0
-t_hours = 0
+tmr_hr, tmr_min, tmr_sec = 0, 0, 0
 
 def timer_update():
-    global t_hours, t_minutes, t_seconds
-    t_seconds -= 1
-    if t_seconds == -1:
-            t_minutes -= 1
-            t_seconds = 59
-    if t_minutes == -1:
-        t_hours -= 1
-        t_minutes = 59
+    global tmr_hr, tmr_min, tmr_sec
+    tmr_sec -= 1
+    if tmr_sec == -1:
+            tmr_min -= 1
+            tmr_sec = 59
+    if tmr_min == -1:
+        tmr_hr -= 1
+        tmr_min = 59
 
-    hours_string = f'{t_hours}' if t_hours > 9 else f'0{t_hours}'
-    minutes_string = f'{t_minutes}' if t_minutes > 9 else f'0{t_minutes}'
-    seconds_string = f'{t_seconds}' if t_seconds > 9 else f'0{t_seconds}'
+    hours_string = f'{tmr_hr}' if tmr_hr > 9 else f'0{tmr_hr}'
+    minutes_string = f'{tmr_min}' if tmr_min > 9 else f'0{tmr_min}'
+    seconds_string = f'{tmr_sec}' if tmr_sec > 9 else f'0{tmr_sec}'
 
     timer_label.config(text = hours_string + ':' + minutes_string + ':' + seconds_string)
     if running:
         global timer_after
         timer_after = timer_label.after(1000, timer_update)
-        if t_hours == 0 and t_minutes == 0 and t_seconds == 0:
+        if tmr_hr == 0 and tmr_min == 0 and tmr_sec == 0:
             stop()
             timer_label.config(text = "Time's up")
             timer_label.after_cancel(timer_after)
+            timer_entry.config(state='normal')
+            timer_start.config(state='normal')
 
-def timer_func():
-    # stop()
-    global t_hours, t_minutes, t_seconds
-    # t_hours, t_minutes, t_seconds =  0, 0, 0
-    time = timer_entry.get()
-    t_hours, t_minutes, t_seconds = map(int, time.split(':'))
-    start()
-
-    timer_update()
-
+def timer_func(command):
+    global tmr_hr, tmr_min, tmr_sec
+    if command == 'start':      
+        time = timer_entry.get()
+        tmr_hr, tmr_min, tmr_sec = map(int, time.split(':'))
+        start()
+        timer_update()
+        timer_start.config(state='disabled')
+        timer_stop.config(state='normal')
+        timer_reset.config(state='normal')
+        timer_entry.config(state='disabled')
+        
+    if command == 'continue':
+        start()
+        timer_update()
+        timer_start.config(state='disabled')
+        timer_stop.config(state='normal')
+        timer_reset.config(state='normal')
+        timer_continue.config(state='disabled')
+        
+    if command == 'stop':
+        stop()
+        timer_start.config(state='disabled')
+        timer_stop.config(state='disabled')
+        timer_continue.config(state='normal')
+        timer_label.after_cancel(timer_after)
+        
+    if command == 'reset':
+        stop()
+        timer_label.after_cancel(timer_after)
+        timer_start.config(state='normal')
+        timer_stop.config(state='disabled')
+        timer_continue.config(state='disabled')
+        timer_reset.config(state='disabled')
+        tmr_hr, tmr_min, tmr_sec = 0, 0, 0
+        timer_label.config(text='00:00:00')
+        timer_entry.config(state='normal')
 
 # Timer components
-timer_label = Label(timer_tab, font = 'calibri 15', text = 'Enter time to count down in format 00:01:30')
+timer_label = Label(timer_tab, font = 'calibri 15', text = 'Enter time as HH:MM:SS')
 timer_label.pack()
 timer_entry = Entry(timer_tab, font = 'calibri 15')
 timer_entry.pack()
-timer_start = Button(timer_tab, text='Start', state = 'normal', command = timer_func)
+timer_start = Button(timer_tab, text='start', state = 'normal', command = lambda: timer_func('start'))
 timer_start.pack()
-timer_stop = Button(timer_tab, text ='Stop', state ='disabled', command = lambda: timer_buttons_func('stop'))
+timer_continue = Button(timer_tab, text='continue', state = 'disabled', command = lambda: timer_func('continue'))
+timer_continue.pack()
+timer_stop = Button(timer_tab, text ='stop', state ='disabled', command = lambda: timer_func('stop'))
 timer_stop.pack()
-timer_reset = Button(timer_tab, text ='Reset', state ='disabled', command = lambda: timer_buttons_func('reset'))
+timer_reset = Button(timer_tab, text ='reset', state ='disabled', command = lambda: timer_func('reset'))
 timer_reset.pack()
+
 
 # Exit button
 exit_btn = ttk.Button(
@@ -209,5 +229,5 @@ def exit_func(event):
     root.quit()
 root.bind('<Return>', exit_func)
 
-time_func()
+clock_func()
 mainloop()
